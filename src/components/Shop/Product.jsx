9472265ff22127524eb4b7f229bdf62ec3270656
jihad-hossain1 <jihadkhan934@gmail.com>
2023-03-28 12:44:55 +0600

    product cart fuction added

--- src/components/Shop/Product.jsx
+++ src/components/Shop/Product.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const Product = (props) => {
-    console.log(props.produc);
-    const { id, img, name, quantity, ratings, seller, stock, price } = props.produc;
+    // console.log(props.produc);
+    // console.log(props.product);
+    const { id, img, name, quantity, ratings, seller, stock, price } = props.product;
+
+    const handleAddToCart = props.handleAddToCart;
     return (
         <div className="m-2">
             <div className="card w-full h-full bg-base-100 shadow-xl relative">
                 <figure><img className="m-2 rounded" src={img} alt="Shoes" /></figure>
-                <div className="card-body ">
+                <div className="card-body my-3">
                         <h2 className="card-title"> {name}</h2>
                         <p><span className=''>Price:</span> $ {price}</p>
                         <p>Seller: {seller}</p>
@@ -17,8 +20,7 @@ const Product = (props) => {
                     
                 </div>
                    
-                <button className="rounded-b-lg bg-warning absolute bottom-0 py-2">add to cart</button>                    
-                    
+                <button onClick={() => handleAddToCart(props.product)} className="w-full rounded-b-lg bg-primary absolute bottom-0 py-2">add to cart</button>
             </div>
         </div>
     );
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -7,14 +7,25 @@ const Shop = () => {
         fetch('products.json')
         .then(res => res.json())
         .then(data => setproducts(data))
-    },[])
+    }, [])
+    // const addToCart = (products) => {
+    //     console.log(products);
+    // }
+    const handleAddToCart = (product) => {
+        console.log(product);
+    }
     return (
         <div className="grid md:grid-cols-[4fr,1fr] lg:grid-cols-[4fr,1fr] m-2">
             <div className='products-here'>
                 <h2>Products comming here total: <span className="btn btn-xs">{products.length}</span></h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5">
                     {
-                    products.map(product => <Product produc={product}></Product>)
+                        products.map(product => <Product
+                            product={product}
+                            key={product.id}
+                            // addToCart={addToCart}
+                            handleAddToCart={handleAddToCart}
+                        ></Product>)
                 }
                 </div>
             </div>
