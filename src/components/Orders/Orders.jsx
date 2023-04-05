3908939c6fe981b1d2c51859e6f938239f6adffb
jihad-hossain1 <jihadkhan934@gmail.com>
2023-04-05 22:59:59 +0600

    video-6 items  remove from order

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviewItems from '../ReviewItems/ReviewItems'
+import { removeFromDb } from '../utilities/fakedb';
 
 
 const Orders = () => {
-    const cart = useLoaderData();
+    const savedCart = useLoaderData();
     // console.log(cart);
+    const [cart, setCart] = useState(savedCart);
+    const handleRemoveFromCart = (id) => {
+        const remaing = cart.filter(p => p.id !== id);
+        setCart(remaing);
+        removeFromDb(id)
+    }
     return (
         <div className='grid md:grid-cols-[4fr,1fr] lg:grid-cols-[4fr,1fr] m-2'>
             <div>
-                <h2>Order page- Total prodcuts: {cart.length}</h2>
+                <h2>Order page- Total prodcuts: {savedCart.length}</h2>
                 <div className='md:mx-20 lg:mx-72'>
                     {
-                        cart.map(product => <ReviewItems product={product}></ReviewItems> )
+                        cart.map(product => <ReviewItems product={product} key={product.id} handleRemoveFromCart={handleRemoveFromCart}></ReviewItems> )
                     }
                 </div>
             </div>
--- src/components/ReviewItems/ReviewItems.jsx
+++ src/components/ReviewItems/ReviewItems.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-const ReviewItems = ({ product }) => {
-    console.log(product);
-    const { name, price, shipping, img} = product;
+const ReviewItems = ({ product,handleRemoveFromCart }) => {
+    // console.log(product);
+    const { name, price, shipping, img ,id} = product;
     return (
         <div className='flex md:flex justify-between items-center border m-2 shadow rounded p-3'>
             <div className='flex '>
@@ -13,12 +13,12 @@ const ReviewItems = ({ product }) => {
                 </div>
                 <div className='ml-3'>
                     <h3 className='text-xl font-semibold'>{name}</h3>
-                    <p>Price: <span className='text-yellow-500'>${price}</span></p>
-                    <p>Shiping Charge: <span className='text-yellow-500'>${ shipping}</span></p>
+                    <p className=''>Price: <span className='text-yellow-500'>${price}</span></p>
+                    <p>Shiping Charge: <span className='text-yellow-500 '>${ shipping}</span></p>
                 </div>
             </div>
             <div>
-                <span className='text-red-500 bg-red-200 p-2 rounded-xl mx-4'><FontAwesomeIcon icon={faTrash} /></span>
+                <span onClick={()=>handleRemoveFromCart(id)} className='text-red-500 bg-red-200 p-2 rounded-xl mx-4 cursor-pointer'><FontAwesomeIcon icon={faTrash} /></span>
             </div>
         </div>
     );
