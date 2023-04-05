2e713561c7123604cfeb3931e1ba6dee77c5a32a
jihad-hossain1 <jihadkhan934@gmail.com>
2023-04-05 11:11:51 +0600

    install router, set router , create home component, module 55, video 1 complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../images/Logo.svg'
+import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <>
@@ -7,10 +8,10 @@ const Header = () => {
         <div className="bg-neutral flex justify-between py-3 px-10 items-center">
             <img className='' src={logo} alt="" />
             <div className=" text-white">
-                <a className='mr-3' href="/shop">shop</a>
-                <a className='mr-3' href="/order">order</a>
-                <a className='mr-3' href="/inventory">inventory</a>
-                <a className='mr-3' href="/login">Login</a>
+                <Link className='mr-3' to="/">Shop</Link>
+                <Link className='mr-3' to="/orders">Orders</Link>
+                <Link className='mr-3' to="/inventory">Inventory</Link>
+                <Link className='mr-3' to="/login">Login</Link>
 
             </div>
         </div>
---
+++ src/components/Inventory/Inventory.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            Invertory
+        </div>
+    );
+};
+
+export default Inventory;
