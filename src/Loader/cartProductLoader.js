84d17d9e2bac700949958c76509cea84874c7310
jihad-hossain1 <jihadkhan934@gmail.com>
2023-04-05 12:42:27 +0600

    video-3 dynamically show data on order card

--- src/Loader/cartProductLoader.js
+++ src/Loader/cartProductLoader.js
@@ -1,7 +1,29 @@
+import { getShoppingCart } from "../components/utilities/fakedb";
+
 const cartProductsLoader = async () => {
     const loaddedProducts = await fetch('products.json')
     const products = await loaddedProducts.json();
-    console.log(products);
+    // console.log(products);
+    // if cart data is in database , you have to use async await
+    const storedCart = getShoppingCart();
+    console.log(storedCart);
+    const savedCart = [];
+    for (const id in storedCart) {
+        const addedProduct = products.find(pd => pd.id === id);
+        if (addedProduct) {
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
+
+        }
+    }
+    return savedCart;
+
+    // if you need to send two things
+    // return [products, savedCart]
+    // another options 
+    // return {products, savedCart}
+    // return {products, cart: savedCart}
 
 }
 
--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -3,15 +3,15 @@ import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 
 const Orders = () => {
-    const products = useLoaderData();
-    // console.log(products);
+    const cart = useLoaderData();
+    console.log(cart);
     return (
         <div className='grid md:grid-cols-[4fr,1fr] lg:grid-cols-[4fr,1fr] m-2'>
             <div>
-                <h2>Order page</h2>
+                <h2>Order page- Total prodcuts: {cart.length}</h2>
             </div>
             <div className='py-3 lg:py-6 bg-primary px-2 lg:px-9 rounded sticky top-0'>
-                <Cart cart={[]}></Cart>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
