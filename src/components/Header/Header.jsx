d484394a7e4576d09346852f9d28e180e65d42f2
jihad-hossain1 <jihadkhan934@gmail.com>
2023-04-06 13:26:02 +0600

    navbar for small device

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../images/Logo.svg'
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark} from '@fortawesome/free-solid-svg-icons'
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <>
             
         <div className="bg-neutral flex justify-between py-3 px-10 items-center">
             <img className='' src={logo} alt="" />
-            <div className=" text-white">
-                <Link className='mr-3' to="/">Shop</Link>
-                <Link className='mr-3' to="/orders">Orders</Link>
-                <Link className='mr-3' to="/inventory">Inventory</Link>
-                <Link className='mr-3' to="/login">Login</Link>
-
+                <div onClick={() => setIsOpen(!isOpen)} className='md:hidden'>
+                    <span>{
+                    isOpen == true ? <FontAwesomeIcon className='ml-4 w-4 h-4' icon={faXmark} /> : <FontAwesomeIcon className='ml-4 w-4 h-4' icon={faXmark} />
+                    }</span>
+                    <ul className={`text-white md:static bg-slate-400 rounded ${open ? 'right-3' : 'right-48'}`}>
+                        <li><Link className='mr-3' to="/">Shop</Link></li>
+                        <li><Link className='mr-3' to="/orders">Orders</Link></li>
+                        <li><Link className='mr-3' to="/inventory">Inventory</Link></li>
+                        <li><Link className='mr-3' to="/login">Login</Link></li>
+                    
+                    </ul>      
             </div>
         </div>
         </>
